19490c57829cd7ed4a641f3c072aceadb9e4f997
SaurabhS120 <Saurabh.sonar120@gmail.com>
2020-10-15 10:43:36 +0530

    search, peek and elements methods are added for stack and output saved

--- src/main/java/Stack_Implementation/Stack.java
+++ src/main/java/Stack_Implementation/Stack.java
@@ -1,10 +1,12 @@
 package Stack_Implementation;
 
+import java.util.Enumeration;
+
 class Stack<T>{
     Object elements[];
     int top;
     public Stack(){
-        int top=-1;
+        top=-1;
         elements=new Object[20];
     }
     public void push(T x)throws StackOverflow{
@@ -25,6 +27,35 @@ class Stack<T>{
             return (T)ele;
         }
     }
+    public boolean isEmpty(){
+        return top<0;
+    }
+    public T peek(){
+        return (T)elements[top];
+    }
+    public int search(T key){
+        for (int i=0;i<elements.length;i++){
+            if(elements[i]==key)return i;
+        }
+        return -1;
+    }
+    public Enumeration elements(){
+        Enumeration<T> e=new Enumeration<T>() {
+            Object e[]=elements;
+            int t=top;
+            int i=-1;
+            @Override
+            public boolean hasMoreElements() {
+                return i<t;
+            }
 
+            @Override
+            public T nextElement() {
+                i++;
+                return (T)e[i];
+            }
+        };
+        return e;
+    }
 
 }
--- src/main/java/Stack_Implementation/StackMain.java
+++ src/main/java/Stack_Implementation/StackMain.java
@@ -1,11 +1,35 @@
 package Stack_Implementation;
 
+import java.util.Enumeration;
+
 public class StackMain {
     public static void main(String[] args) {
         Stack<Integer> s=new Stack<>();
         try {
             s.push(2);
-            s.pop();
+            s.push(3);
+            s.push(4);
+            s.push(5);
+            s.push(12);
+            s.push(9);
+            Enumeration e=s.elements();
+            System.out.println("Elements in stack:");
+            while (e.hasMoreElements()){
+                System.out.print("\t"+e.nextElement());
+            }
+            System.out.println("\nposition of 4 in stack:"+s.search(4));
+            e=s.elements();
+            System.out.println("\nthe peek/top most element in stack is: "+s.peek());
+            System.out.println("\nElements in stack:");
+            while (e.hasMoreElements()){
+                System.out.print("\t"+e.nextElement());
+            }
+            System.out.println("\ntop most element"+s.pop()+"is popped");
+            e=s.elements();
+            System.out.println("\nElements in stack:");
+            while (e.hasMoreElements()){
+                System.out.print("\t"+e.nextElement());
+            }
         } catch (StackOverflow stackOverflow) {
             stackOverflow.printStackTrace();
         } catch (StackUnderflow stackUnderflow) {
